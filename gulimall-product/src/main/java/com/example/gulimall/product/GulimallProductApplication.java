421c7ca8800a2fe781461d0e70eb59a25089330e
Perseverance <1445272085@qq.com>
2022-09-16 14:27:55 +0800

    设置mybatisplus相关配置,每个服务设置独立端口

--- gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+@MapperScan("com.example.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/example/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/example/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.example.gulimall.product.entity.BrandEntity;
+import com.example.gulimall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    private BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("华为。。");
+        brandService.updateById(brandEntity);
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功");
     }
 
 }
