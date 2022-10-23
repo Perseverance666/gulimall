b778fc87b404aacd1be817653de36c8975d8918b
Perseverance <1445272085@qq.com>
2022-10-23 21:32:54 +0800

    商城业务-消息队列-Springboot整合RabbitMQ

--- gulimall-order/src/test/java/com/example/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/example/gulimall/order/GulimallOrderApplicationTests.java
@@ -4,10 +4,17 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GulimallOrderApplicationTests {
+public class GulimallOrderApplicationTests {
 
+
+    /**
+     * 1、如何创建Exchange[hello-java-exchange]、Queue、Binding
+     * 1）、使用 AmqpAdmin 进行创建
+     * 2、如何收发消息
+     */
     @Test
-    void contextLoads() {
+    public void createExchange() {
+
     }
 
 }
